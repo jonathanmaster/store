c86b049438a4484ea58cc93800277cbb4e13e82a
jonathan <davidrodriguezcarvajal686@gmail.com>
2021-12-30 17:02:40 -0500

    antes de reorganizar los archivos

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
-import { ShoppingCartService } from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-header',
   template: `
   <mat-toolbar color="primary">
     <span>My Store</span>
-    {{quantity$ | async | json}} - Quantity
-    {{total$ | async | json}} - Total
-  </mat-toolbar>
-  `,
+    <span class="spacer"></span>
+    <app-cart></app-cart>
+  </mat-toolbar>`,
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
-  quantity$ = this.shoppingCartSvc.quantityAction$;
-  total$ = this.shoppingCartSvc.totalAction$;
-  cart$ = this.shoppingCartSvc.cartAction$;
-  constructor(private shoppingCartSvc: ShoppingCartService) { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HeaderComponent {}
